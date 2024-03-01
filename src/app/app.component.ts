2744aece64bd13d2b954b26f4b458dacb62dbdda
deBeee <fiacik12345@gmail.com>
2024-03-01 12:35:55 +0100

    created angular project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,36 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { TasksListComponent } from './tasks-list.component';
+import { SubmitTextComponent } from './submit-text.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [TasksListComponent, SubmitTextComponent],
+
+  template: `
+    <h1 class="text-orange-500 bg-black py-4 text-xl text-center mb-4">
+      Another boring todolist
+    </h1>
+    <app-submit-text (submitText)="addTask($event)" />
+    <app-tasks-list [tasks]="tasks" />
+  `,
 })
 export class AppComponent {
-  title = 'angular-todolist';
+  tasks = [
+    {
+      name: 'Angular introduction',
+      done: false,
+    },
+    {
+      name: 'Learn components',
+      done: true,
+    },
+  ];
+
+  addTask(name: string) {
+    this.tasks.push({
+      name,
+      done: false,
+    });
+  }
 }
