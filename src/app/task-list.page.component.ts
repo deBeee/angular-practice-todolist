13335fa898afa1e82338fd7f0f0535d72dc40a5c
deBeee <fiacik12345@gmail.com>
2024-03-01 16:01:32 +0100

    covering unhappy path scenario where external server encounters an error (api error handling)

--- src/app/task-list.page.component.ts
+++ src/app/task-list.page.component.ts
@@ -4,6 +4,8 @@ import { SubmitTextComponent } from './submit-text.component';
 import { Task } from './Task';
 import { NgIf } from '@angular/common';
 
+type ListFetchingError = { status: number; message: string };
+
 @Component({
   selector: 'app-task-list-page',
   standalone: true,
@@ -16,6 +18,8 @@ import { NgIf } from '@angular/common';
       [tasks]="tasks"
     />
 
+    <p *ngIf="error">{{ error.message }}</p>
+
     <ng-template #loadingTemplate>
       <p>Loading...</p>
     </ng-template>
@@ -25,18 +29,25 @@ export class TaskListPageComponent {
   tasks: Task[] = [];
 
   loading = false;
-
+  error?: ListFetchingError;
   private readonly URL = 'http://localhost:3000';
 
   constructor() {
     this.loading = true;
     fetch(`${this.URL}/tasks`)
-      .then<Task[]>((response) => {
-        return response.json();
+      .then<Task[] | ListFetchingError>((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        return { status: response.status, message: response.statusText };
       })
       .then((response) => {
         setTimeout(() => {
-          this.tasks = response;
+          if (Array.isArray(response)) {
+            this.tasks = response;
+          } else {
+            this.error = response;
+          }
           this.loading = false;
         }, 1200);
       });
