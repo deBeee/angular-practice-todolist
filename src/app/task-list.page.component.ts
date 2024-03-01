0f7561080181da2272e07fa00b68b8897fa47f8d
deBeee <fiacik12345@gmail.com>
2024-03-01 15:40:17 +0100

    getting data from api using fetch, added settimeout to simulate server extended answering time and conditionally rendering using ng-template to cover every possible application state

--- src/app/task-list.page.component.ts
+++ src/app/task-list.page.component.ts
@@ -1,32 +1,51 @@
 import { Component } from '@angular/core';
 import { TasksListComponent } from './tasks-list.component';
 import { SubmitTextComponent } from './submit-text.component';
+import { Task } from './Task';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-task-list-page',
   standalone: true,
-  imports: [TasksListComponent, SubmitTextComponent],
+  imports: [TasksListComponent, SubmitTextComponent, NgIf],
   template: `
     <app-submit-text (submitText)="addTask($event)" />
-    <app-tasks-list class="block mt-4" [tasks]="tasks" />
+    <app-tasks-list
+      *ngIf="!loading; else loadingTemplate"
+      class="block mt-4"
+      [tasks]="tasks"
+    />
+
+    <ng-template #loadingTemplate>
+      <p>Loading...</p>
+    </ng-template>
   `,
 })
 export class TaskListPageComponent {
-  tasks = [
-    {
-      name: 'Angular introduction',
-      done: false,
-    },
-    {
-      name: 'Learn components',
-      done: true,
-    },
-  ];
+  tasks: Task[] = [];
+
+  loading = false;
+
+  private readonly URL = 'http://localhost:3000';
+
+  constructor() {
+    this.loading = true;
+    fetch(`${this.URL}/tasks`)
+      .then<Task[]>((response) => {
+        return response.json();
+      })
+      .then((response) => {
+        setTimeout(() => {
+          this.tasks = response;
+          this.loading = false;
+        }, 1200);
+      });
+  }
 
   addTask(name: string) {
-    this.tasks.push({
-      name,
-      done: false,
-    });
+    // this.tasks.push({
+    //   name,
+    //   done: false,
+    // });
   }
 }
