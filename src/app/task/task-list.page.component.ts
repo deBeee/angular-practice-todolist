e702eb82fa2cae57f8eaf3ca70cabf8313755856
deBeee <fiacik12345@gmail.com>
2024-03-05 00:10:03 +0100

    extracted form with task list filters to new component. Separated adapters, enums and types

--- src/app/task/task-list.page.component.ts
+++ src/app/task/task-list.page.component.ts
@@ -4,195 +4,72 @@ import { SubmitTextComponent } from '../shared/ui/submit-text.component';
 import { Task } from './model/Task';
 import { NgIf } from '@angular/common';
 import { GetAllTasksSearchParams, TasksService } from './data-access/tasks.service';
-import { ComponentListState } from '../utils/list-state.type';
-import { SORT_BY, SortBy } from '../shared/enums/sort-by.enum';
+import { ComponentListState, LIST_STATE_VALUE } from '../utils/list-state.type';
 import {
-  FormControl,
-  FormGroup,
-  NonNullableFormBuilder,
-  ReactiveFormsModule,
-} from '@angular/forms';
-
-export const TODO_STATUS = {
-  ALL: 'ALL',
-  TODO: 'TODO',
-  DONE: 'DONE',
-} as const;
-
-type TodoStatus = keyof typeof TODO_STATUS;
-
-type TasksListFiltersForm = FormGroup<{
-  searchTerm: FormControl<string>;
-  status: FormControl<TodoStatus>;
-  sortBy: FormControl<SortBy>;
-}>;
-
-type TasksListFiltersFormValue = ReturnType<TasksListFiltersForm['getRawValue']>;
-
-function getAllTasksSearchParams(
-  formValue: TasksListFiltersFormValue,
-): GetAllTasksSearchParams {
-  let searchParams = {
-    _sort: 'createdAt',
-    _order: formValue.sortBy.toLocaleLowerCase(),
-    q: formValue.searchTerm,
-  } as GetAllTasksSearchParams;
-
-  if (formValue.status === 'TODO') {
-    searchParams.done_like = 'false';
-  } else if (formValue.status === 'DONE') {
-    searchParams.done_like = 'true';
-  } else {
-    searchParams.done_like = '';
-  }
-
-  return searchParams;
-}
+  TasksListFiltersComponent,
+  TasksListFiltersFormValue,
+} from './ui/task-list-filters.component';
+import { getAllTasksSearchParams } from './data-access/tasks-filters.adapter';
 
 @Component({
   selector: 'app-task-list-page',
   standalone: true,
-  imports: [TasksListComponent, SubmitTextComponent, NgIf, ReactiveFormsModule],
-  styles: [
-    `
-      details {
-        border: 1px solid #aaa;
-        border-radius: 4px;
-        padding: 0.5em 0.5em 0;
-        width: 300px;
-        margin: 1em 0;
-      }
-
-      summary {
-        font-weight: bold;
-        margin: -0.5em -0.5em 0;
-        padding: 0.5em;
-      }
-
-      summary:before {
-        content: '👉';
-        display: inline-block;
-        font-size: 0.8em;
-        padding: 0 0.5em;
-        transition: transform 0.2s ease-in-out;
-      }
-
-      details[open] > summary:before {
-        transform: rotate(90deg);
-      }
-    `,
-  ],
+  imports: [TasksListComponent, SubmitTextComponent, NgIf, TasksListFiltersComponent],
   template: `
     <app-submit-text
-      (submitText)="listState.state === 'success' && addTask($event, listState.results)"
+      (submitText)="
+        listState.state === listStateValue.SUCCESS && addTask($event, listState.results)
+      "
     />
-    <form [formGroup]="form">
-      <details open>
-        <summary class="marker:content-none">Filters</summary>
-        <div class="mb-2 flex justify-between">
-          <fieldset class="flex flex-col">
-            <label for="filter-search-term">Search</label>
-            <input
-              formControlName="searchTerm"
-              id="filter-search-term"
-              class="border-b border-b-orange-400 outline-none"
-            />
-          </fieldset>
-
-          <fieldset class="flex flex-col">
-            <label for="filter-status">Status</label>
-            <select
-              formControlName="status"
-              id="filter-status"
-              class="border border-orange-400 outline-none"
-            >
-              <option [value]="statusOptions.ALL">-</option>
-              <option [value]="statusOptions.DONE">Done</option>
-              <option [value]="statusOptions.TODO">Todo</option>
-            </select>
-          </fieldset>
-        </div>
-      </details>
-      <div class="flex gap-12">
-        <button
-          class="rounded px-2 py-1 transition-colors duration-300"
-          [class.bg-green-400]="form.controls.sortBy.value === sortOptions.DESC"
-          (click)="sort(sortOptions.DESC)"
-        >
-          📆🔽
-        </button>
-        <button
-          class="rounded px-2 py-1 transition-colors duration-300"
-          [class.bg-green-400]="form.controls.sortBy.value === sortOptions.ASC"
-          (click)="sort(sortOptions.ASC)"
-        >
-          📆🔼
-        </button>
-      </div>
-    </form>
+    <app-tasks-list-filters (filtersChange)="handleFiltersChange($event)" />
     <app-tasks-list
-      *ngIf="listState.state === 'success'"
+      *ngIf="listState.state === listStateValue.SUCCESS"
       class="block mt-4"
       [tasks]="listState.results"
     />
-    <p *ngIf="listState.state === 'error'">{{ listState.error.message }}</p>
-    <p *ngIf="listState.state === 'loading'">Loading...</p>
+    <p *ngIf="listState.state === listStateValue.ERROR">
+      {{ listState.error.message }}
+    </p>
+    <p *ngIf="listState.state === listStateValue.LOADING">Loading...</p>
   `,
 })
 export class TaskListPageComponent {
-  listState: ComponentListState<Task> = { state: 'idle' };
-
   private tasksService = inject(TasksService);
-  private formBuilder = inject(NonNullableFormBuilder);
 
-  form: TasksListFiltersForm = this.formBuilder.group({
-    searchTerm: this.formBuilder.control<string>('learn'),
-    sortBy: this.formBuilder.control<SortBy>('ASC'),
-    status: this.formBuilder.control<TodoStatus>(TODO_STATUS.ALL),
-  });
-
-  sortOptions = SORT_BY;
-  statusOptions = TODO_STATUS;
-
-  sortedBy: SortBy = SORT_BY.DESC;
-
-  sort(sort: SortBy) {
-    this.form.patchValue({
-      sortBy: sort,
-    });
-  }
+  listState: ComponentListState<Task> = { state: LIST_STATE_VALUE.IDLE };
+  listStateValue = LIST_STATE_VALUE;
 
   ngOnInit() {
-    this.listState = { state: 'loading' };
-
-    this.form.valueChanges.subscribe(() => {
-      this.getAllTasks(getAllTasksSearchParams(this.form.getRawValue()));
-    });
-
-    this.getAllTasks(getAllTasksSearchParams(this.form.getRawValue()));
+    //this.getAllTasks(getAllTasksSearchParams(this.form.getRawValue()));
   }
 
-  getAllTasks(searchParams: GetAllTasksSearchParams) {
+  handleFiltersChange(filters: TasksListFiltersFormValue): void {
+    this.getAllTasks(getAllTasksSearchParams(filters));
+  }
+
+  getAllTasks(searchParams: GetAllTasksSearchParams): void {
+    this.listState = { state: LIST_STATE_VALUE.LOADING };
+
     this.tasksService.getAll(searchParams).then((response) => {
       if (Array.isArray(response)) {
         this.listState = {
-          state: 'success',
+          state: LIST_STATE_VALUE.SUCCESS,
           results: response,
         };
       } else {
         this.listState = {
-          state: 'error',
+          state: LIST_STATE_VALUE.ERROR,
           error: response,
         };
       }
     });
   }
 
-  addTask(name: string, tasks: Task[]) {
+  addTask(name: string, tasks: Task[]): void {
     this.tasksService.add(name).then((response) => {
       if ('id' in response) {
         this.listState = {
-          state: 'success',
+          state: LIST_STATE_VALUE.SUCCESS,
           results: tasks.concat(response),
         };
       } else {
--- src/app/utils/list-state.type.ts
+++ src/app/utils/list-state.type.ts
@@ -1,25 +1,35 @@
-import { Task } from '../task/model/Task';
-
 export type ListFetchingError = { status: number; message: string };
 
 // idle - initial
 type IdleState = {
-  state: 'idle';
+  state: LIST_STATE_VALUE['IDLE'];
 };
 // loading
 type LoadingState = {
-  state: 'loading';
+  state: LIST_STATE_VALUE['LOADING'];
 };
 // success
 type SuccessState<T> = {
-  state: 'success';
+  state: LIST_STATE_VALUE['SUCCESS'];
   results: T[];
 };
 // error
 type ErrorState = {
-  state: 'error';
+  state: LIST_STATE_VALUE['ERROR'];
   error: ListFetchingError;
 };
+
+type LIST_STATE_VALUE = typeof LIST_STATE_VALUE;
+
+export const LIST_STATE_VALUE = {
+  IDLE: 'IDLE',
+  LOADING: 'LOADING',
+  SUCCESS: 'SUCCESS',
+  ERROR: 'ERROR',
+} as const;
+
+export type ListStateValue = keyof typeof LIST_STATE_VALUE;
+
 export type ComponentListState<T> =
   | IdleState
   | LoadingState
