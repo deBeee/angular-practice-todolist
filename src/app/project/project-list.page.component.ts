bddb1242853779aad79e01417e3eccfcae2aa7cf
deBeee <fiacik12345@gmail.com>
2024-03-06 12:37:56 +0100

    added project-list page component

---
+++ src/app/project/project-list.page.component.ts
@@ -0,0 +1,78 @@
+import { Component, inject } from '@angular/core';
+import { NgFor, NgIf } from '@angular/common';
+import { SubmitTextComponent } from '../shared/ui/submit-text.component';
+import { ComponentListState, LIST_STATE_VALUE } from '../utils/list-state.type';
+import { Project } from './model/Project';
+import { ProjectsApiService } from './data-access/projects.api.service';
+@Component({
+  selector: 'app-project-list-page',
+  standalone: true,
+  imports: [SubmitTextComponent, NgIf, NgFor],
+  template: `
+    <p>Projects:</p>
+    <app-submit-text
+      (submitText)="
+        listState.state === listStateValue.SUCCESS &&
+          addProject($event, listState.results)
+      "
+    />
+
+    <ol
+      class="list-decimal list-inside"
+      *ngIf="listState.state === listStateValue.SUCCESS"
+    >
+      <li *ngFor="let project of listState.results">{{ project.name }}</li>
+    </ol>
+
+    <p *ngIf="listState.state === listStateValue.ERROR">
+      {{ listState.error.message }}
+    </p>
+    <p *ngIf="listState.state === listStateValue.LOADING">Loading...</p>
+  `,
+})
+export class ProjectListPageComponent {
+  private projectsApiService = inject(ProjectsApiService);
+
+  listState: ComponentListState<Project> = { state: LIST_STATE_VALUE.IDLE };
+  listStateValue = LIST_STATE_VALUE;
+
+  ngOnInit() {
+    this.getAllProjects();
+  }
+
+  getAllProjects(): void {
+    this.listState = { state: LIST_STATE_VALUE.LOADING };
+
+    this.projectsApiService.getAll().subscribe({
+      next: (projects) => {
+        this.listState = {
+          state: LIST_STATE_VALUE.SUCCESS,
+          results: projects,
+        };
+      },
+      error: (err) => {
+        this.listState = {
+          state: LIST_STATE_VALUE.ERROR,
+          error: err,
+        };
+      },
+    });
+  }
+
+  addProject(name: string, projects: Project[]): void {
+    this.projectsApiService.add(name).subscribe({
+      next: (project) => {
+        this.listState = {
+          state: LIST_STATE_VALUE.SUCCESS,
+          results: projects.concat(project),
+        };
+      },
+      error: (err) => {
+        this.listState = {
+          state: LIST_STATE_VALUE.ERROR,
+          error: err,
+        };
+      },
+    });
+  }
+}
